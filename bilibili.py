836082124192d80dc66e886d5aa387dc592f03f1
ezshine <ezshine@live.cn>
2022-01-05 22:13:53 +0800

    add tts and danmu system

--- bilibili.py
+++ bilibili.py
@@ -2,31 +2,43 @@ import sys
 import os
 import pygame
 import requests
-import threading
+from threading import Thread
 import time
 import socket
+import pyttsx3
+import httpx
+import asyncio
 
 # 全局变量
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 400
-BG_COLOR = (0,0,0)
 TIME_SECOND = 0
 CUR_PATH = os.path.split(os.path.realpath(__file__))[0]
 FONT_PATH = CUR_PATH+'/font.ttf'
 
-# bilibili
+# 屏幕尺寸和画布背景色
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 400
+BG_COLOR = (0,0,0)
+
+# 轮询间隔
 REQUEST_INTERVAL = 5
 
+# 哔哩哔哩配置
 BILI_MID = "422646817"
 BILI_LIVEID = "21759271"
 BILI_SESSDATA = "3ea2cef8%2C1656552588%2C5d28d%2A11"
 
+# bilibili
 BILI_UNREAD = 0  #未读消息
 BILI_LIVEONLINE = 0  #直播人气
 BILI_TOTALFANS = 0  #粉丝数
 BILI_TOTALVIEW = 0  #总播放
 BILI_TOTALLIKE = 0 #总获赞
 BILI_TOTALELEC = 0 #总充电
+
+DANMU_ISSHOW = 0
+DANMU_ISRUNNING = 0
+DANMU_TEXT = ''
+DANMU_X = 0
 
 def get_host_ip():
     try:
@@ -38,7 +50,7 @@ def get_host_ip():
  
     return ip
 
-def getBilibiliFansCount():
+async def getBilibiliFansCount():
     global BILI_TOTALFANS
     global BILI_UNREAD
     global BILI_LIVEONLINE
@@ -46,27 +58,29 @@ def getBilibiliFansCount():
     global BILI_TOTALLIKE
     global BILI_TOTALVIEW
     try:
-        res = requests.get('https://api.bilibili.com/x/relation/stat?vmid='+BILI_MID)
-        data = res.json()
-        
-        BILI_TOTALFANS = data['data']['follower']
+        async with httpx.AsyncClient() as client:
+            res = await client.get('https://api.bilibili.com/x/relation/stat?vmid='+BILI_MID)
+            data = res.json()
+            BILI_TOTALFANS = data['data']['follower']
     except:
         BILI_TOTALFANS = 0
 
     try:
-        res = requests.get('http://api.bilibili.com/x/msgfeed/unread',headers={
-            'Cookie':'SESSDATA='+BILI_SESSDATA
-        })
-        data = res.json()
-        BILI_UNREAD = data['data']['at']+data['data']['chat']+data['data']['like']+data['data']['reply']+data['data']['sys_msg']+data['data']['up']
+        async with httpx.AsyncClient() as client:
+            res = await client.get('http://api.bilibili.com/x/msgfeed/unread',headers={
+                'Cookie':'SESSDATA='+BILI_SESSDATA
+            })
+            data = res.json()
+            BILI_UNREAD = data['data']['at']+data['data']['chat']+data['data']['like']+data['data']['reply']+data['data']['sys_msg']+data['data']['up']
     except:
         BILI_UNREAD = 0
 
     try:
-        res = requests.get('https://api.bilibili.com/x/space/acc/info?mid='+BILI_MID)
-        data = res.json()
-        BILI_LIVEID = data['data']['live_room']['roomid']
-        BILI_LIVEONLINE = data['data']['live_room']['online']
+        async with httpx.AsyncClient() as client:
+            res = requests.get('https://api.bilibili.com/x/space/acc/info?mid='+BILI_MID)
+            data = res.json()
+            BILI_LIVEID = data['data']['live_room']['roomid']
+            BILI_LIVEONLINE = data['data']['live_room']['online']
     except:
         BILI_LIVEONLINE = 0
 
@@ -85,26 +99,59 @@ def getBilibiliFansCount():
     #     BILI_TOTALLIKE = 0
 
     try:
-        res = requests.get('https://api.bilibili.com/x/ugcpay-rank/elec/month/up?up_mid='+BILI_MID)
-        data = res.json()
-        BILI_TOTALELEC = data['data']['total']
+        async with httpx.AsyncClient() as client:
+            res = await client.get('https://api.bilibili.com/x/ugcpay-rank/elec/month/up?up_mid='+BILI_MID)
+            data = res.json()
+            BILI_TOTALELEC = data['data']['total']
     except:
         BILI_TOTALELEC = 0
 
 def getLiveRoomChat():
-    # https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid=
-    print(BILI_LIVEID)
+    global DANMU_ISSHOW
+    global DANMU_LAST
+    global DANMU_TEXT
     try:
         res = requests.get('https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid='+BILI_LIVEID)
         data = res.json()
-        
-        print(data['data']['room'])
-    except:
-        print("报错了")
+
+        room = data['data']['room']
+        if len(room)>0:
+            last_danmu = room[len(room)-1]
+            last_danmu_time = time.mktime(time.strptime(last_danmu['timeline'], "%Y-%m-%d %H:%M:%S"))
+            if 'DANMU_LAST' in globals():
+                if DANMU_LAST == last_danmu:
+                    return None
+            if last_danmu_time>time.time()-300 or last_danmu['uid']!=BILI_MID:
+                DANMU_LAST = last_danmu
+                DANMU_TEXT = last_danmu['text']
+                DANMU_ISSHOW = 1
+    except Exception as e:
+        print(e)
+
+def draw_danmu():
+    global DANMU_ISRUNNING
+    global DANMU_ISSHOW
+    global DANMU_X
+    global DANMU_TEXT
+
+    if DANMU_ISRUNNING==1:
+        my_font = pygame.font.Font(FONT_PATH, 220)
+        text_fmt = my_font.render(DANMU_TEXT, 1, (255,255,255))
+        text_width, text_height = my_font.size(DANMU_TEXT)
+        screen.blit(text_fmt, (DANMU_X,60))
+        DANMU_X-=2
+        if DANMU_X<-text_width:
+            DANMU_ISSHOW = 0
+            DANMU_ISRUNNING = 0
+    else:
+        DANMU_ISRUNNING = 1
+        DANMU_X = SCREEN_WIDTH
+        Thread(target=pyttsx3.speak,args=(DANMU_TEXT,)).start()
+
 
 def requestBiliData():
-    getBilibiliFansCount()
-    # getLiveRoomChat()
+    asyncio.run(getBilibiliFansCount())
+    getLiveRoomChat()
 
 def draw_time():
     date_str = time.strftime("%Y 年 %m 月 %d 日", time.localtime())
@@ -147,7 +194,7 @@ def draw_bilibili():
     text_width, text_height = my_font.size("未读消息："+str(BILI_UNREAD))
     screen.blit(text_fmt, (10,SCREEN_HEIGHT-10-text_height))
 
-    totalstr = " 总充电："+str(BILI_TOTALELEC)+" 直播人气："+str(BILI_LIVEONLINE)
+    totalstr = " 总充电："+str(BILI_TOTALELEC)+" 直播间人气："+str(BILI_LIVEONLINE)
     my_font = pygame.font.Font(FONT_PATH, 20)
     text_fmt = my_font.render(totalstr, 1, (255,255,255))
     text_width, text_height = my_font.size(totalstr)
@@ -158,20 +205,27 @@ def game_loop():
     screen.fill(BG_COLOR)
 
     global TIME_SECOND
+    global DANMU_ISSHOW
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
+            print("quit")
+        if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit()
         if event.type == SECOND_EVT:
             TIME_SECOND+=1
-            if TIME_SECOND%REQUEST_INTERVAL==0:
+            if TIME_SECOND%REQUEST_INTERVAL==0 and DANMU_ISSHOW==0:
                 requestBiliData()
             
     # 视图层构建区
 
-    draw_time()
-    draw_ip()
-    draw_bilibili()
+    if DANMU_ISSHOW==1:
+        draw_danmu()
+    else:
+        draw_time()
+        draw_ip()
+        draw_bilibili()
     
     # 刷新识图
     pygame.display.update()
@@ -179,6 +233,8 @@ def game_loop():
 def run_game():
     pygame.init()
     pygame.mouse.set_visible(0)
+
+    fpsClock = pygame.time.Clock()
 
     global screen
     screen = pygame.display.set_mode((0, 0), pygame.NOFRAME)
@@ -195,8 +251,14 @@ def run_game():
 
     requestBiliData()
 
+    # global DANMU_ISSHOW
+    # global DANMU_TEXT
+    # DANMU_ISSHOW = 1
+    # DANMU_TEXT = "哈哈哈哈哈"
+
     # game loop
     while True:
         game_loop()
+        fpsClock.tick(60)
 
 run_game()
---
+++ speak.py
@@ -0,0 +1,5 @@
+import sys
+import pyttsx3
+
+
+pyttsx3.speak(str(sys.argv[1]))
